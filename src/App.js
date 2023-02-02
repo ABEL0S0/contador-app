825581f5b64e2b42295a857b250bbf16fa0945aa
ABEL0S0 <avilaospinabel@gmail.com>
2023-02-02 13:00:32 -0500

    Contador_Clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Contador from './components/Contador';
 
 function App() {
+  const [valor, setValor]=useState(0);
+
+  const incrementarHandler=()=>{
+    console.log("Incrementando");
+    setValor(valor+1)
+  }
+  const DecrementarHandler=()=>{
+    console.log("Decrementando");
+    setValor(valor-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <Contador valor={valor}/>
+      <div>
+        <button className='botn-size boton-mas ' onClick={incrementarHandler}>+</button>
+        <button className='botn-size boton-reiniciar' onClick={()=> setValor(0)}>Clear</button>
+        <button className='botn-size boton-menos' onClick={DecrementarHandler}>-</button>
+      </div>
     </div>
   );
 }
